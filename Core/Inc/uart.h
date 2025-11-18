48e6a3f2d6a3320934689aa3a44fb9037d2f52bf
CZH <CZH>
2025-11-18 15:46:52 +0800

    add uart1

---
+++ Core/Inc/uart.h
@@ -0,0 +1,20 @@
+#ifndef __UART_H
+#define __UART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+  * @brief USART1初始化函数声明
+  * @param None
+  * @retval None
+  */
+void MX_USART1_UART_Init(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UART_H */
